2744e827624d7f7ec0635e9d155d423435caca1b
Stella Annor <stella.annor@icloud.com>
2023-04-24 16:57:29 +0100

    initial commit

---
+++ src/main/java/com/example/continent_game/ContinentGameApplication.java
@@ -0,0 +1,13 @@
+package com.example.continent_game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContinentGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContinentGameApplication.class, args);
+	}
+
+}
