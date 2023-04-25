dcf112df987df39d348665aa412e8647f2ec5a3e
Khalid Hussein <khalhussi@gmail.com>
2023-04-25 10:01:27 +0100

    edited models

--- src/main/java/com/example/continent_game/models/Continent.java
+++ src/main/java/com/example/continent_game/models/Continent.java
@@ -1,4 +1,56 @@
 package com.example.continent_game.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Continent {
+    private Long id;
+    private String name;
+    private List<Country> countryList;
+    private List<Game> gameList;
+    public Continent (String name){
+        this.name = name;
+        this.countryList = new ArrayList<>();
+        this.gameList = new ArrayList<>();
+    }
+    public Continent(){
+
+    }
+//    Methods
+
+
+//    Getters/Setters
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Country> getCountryList() {
+        return countryList;
+    }
+
+    public void setCountryList(List<Country> countryList) {
+        this.countryList = countryList;
+    }
+
+    public List<Game> getGameList() {
+        return gameList;
+    }
+
+    public void setGameList(List<Game> gameList) {
+        this.gameList = gameList;
+    }
 }
--- src/main/java/com/example/continent_game/models/Game.java
+++ src/main/java/com/example/continent_game/models/Game.java
@@ -1,4 +1,86 @@
 package com.example.continent_game.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+@Entity
+@Table(name = "games")
 public class Game {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+    @Column
+    private int score;
+    @Column
+    private boolean complete;
+    @JsonIgnoreProperties({"chocolates"})
+    @OneToMany
+            name = "chocolates_estates",
+            joinColumns = @JoinColumn(name = "chocolate_id"),
+            inverseJoinColumns = @JoinColumn(name = "estate_id")
+    )
+    private List<Continent> continentList;
+    @JsonIgnoreProperties({"chocolates"})
+    @ManyToMany
+    @JoinTable(
+            name = "chocolates_estates",
+            joinColumns = @JoinColumn(name = "chocolate_id"),
+            inverseJoinColumns = @JoinColumn(name = "estate_id")
+    )
+    private List<Player> playerList;
+
+    public Game (int score, boolean complete){
+        this.score = score;
+        this.complete = complete;
+        this.continentList = new ArrayList<>();
+        this.playerList = new ArrayList<>();
+    }
+//    Methods
+
+
+//    Getters/Setters
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public boolean isComplete() {
+        return complete;
+    }
+
+    public void setComplete(boolean complete) {
+        this.complete = complete;
+    }
+
+    public List<Continent> getContinentList() {
+        return continentList;
+    }
+
+    public void setContinentList(List<Continent> continentList) {
+        this.continentList = continentList;
+    }
+
+    public List<Player> getPlayerList() {
+        return playerList;
+    }
+
+    public void setPlayerList(List<Player> playerList) {
+        this.playerList = playerList;
+    }
 }
