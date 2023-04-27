8ffe5c5feb814963a31b261219f5c8226cfe0262
klb545 <126502552+klb545@users.noreply.github.com>
2023-04-27 15:25:47 +0100

    removed unnecessary commented out code from dataloader

--- src/main/java/com/example/continent_game/components/DataLoader.java
+++ src/main/java/com/example/continent_game/components/DataLoader.java
@@ -414,39 +414,6 @@ public class DataLoader implements ApplicationRunner {
         }
 
 
-//        CONTINENT: String name,
-//        COUNTRY: String name, Long GDP, String capital, int population, Continent continent)
-
-
-////        CONTINENTS
-//        Continent continentAfrica = new Continent("Africa");
-//        continentRepository.save(continentAfrica);
-//        Continent continentEurope = new Continent("Europe");
-//        continentRepository.save(continentEurope);
-//        Continent continentNorthAmerica = new Continent("NorthAmerica");
-//        continentRepository.save(continentNorthAmerica);
-//
-//
-//        ////        CONTINENT: String name,
-//////        COUNTRY: String name, Long GDP, String capital, int population, Continent, continent)
-//
-////        COUNTRIES IN EUROPE
-//        Country countryUnitedKingdom = new Country("United Kingdom", 3100000000000L, "London", 67330000, continentEurope);
-//        Country countryFrance= new Country("France", 5100000000000L, "France", 100330000, continentEurope);
-//        Country countryIreland= new Country("Ireland", 300000000000L, "Dublin", 100330000, continentEurope);
-//        countryRepository.save(countryUnitedKingdom);
-//        countryRepository.save(countryFrance);
-//        countryRepository.save(countryIreland);
-//
-////        COUNTRIES IN AFRICA
-//        Country countryEgypt = new Country("Egypt", 3100000000000L, "Cairo", 67330000, continentAfrica);
-//        Country countryDjibouti= new Country("Djibouti", 5100000000000L, "Djibouti", 100330000, continentAfrica);
-//        Country countrySouthAfrica= new Country("SouthAfrica", 300000000000L, "Johannesburg??", 100330000, continentAfrica);
-//        countryRepository.save(countryEgypt);
-//        countryRepository.save(countryDjibouti);
-//        countryRepository.save(countrySouthAfrica);
-
-
     }
 
 
